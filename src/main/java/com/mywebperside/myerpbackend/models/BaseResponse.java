f77c30e402670d98daf2df7eeb25409826bcdad3
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2025-12-24 21:21:09 +0400

    finish initialize

--- src/main/java/com/mywebperside/myerpbackend/models/BaseResponse.java
+++ src/main/java/com/mywebperside/myerpbackend/models/BaseResponse.java
@@ -2,33 +2,38 @@ package com.mywebperside.myerpbackend.models;
 
 
 import org.springframework.http.HttpStatus;
-
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//public class BaseResponse<T> {
-//    HttpStatus status;
-//    String msg;
-//    T data;
-//
-//    public static <T> BaseResponse<T> success(T data) {
-//        return BaseResponse.<T>builder()
-//                .status(HttpStatus.OK)
-//                .msg("Success")
-//                .build();
-//    }
-//}
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
-public record BaseResponse<T>(HttpStatus status, String msg, T data) {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BaseResponse<T> {
+    HttpStatus status;
+    String msg;
+    T data;
 
     public static <T> BaseResponse<T> success(T data) {
-        return new BaseResponse<>(HttpStatus.OK, "Success", data);
-    }
-
-    public static <T> BaseResponse<T> error(HttpStatus status, String msg) {
-        return new BaseResponse<>(status, msg, null);
+        return BaseResponse.<T>builder()
+                .status(HttpStatus.OK)
+                .msg("Success")
+                .build();
     }
 }
+
+
+//public record BaseResponse<T>(HttpStatus status, String msg, T data) {
+//
+//    public static <T> BaseResponse<T> success(T data) {
+//        return new BaseResponse<>(HttpStatus.OK, "Success", data);
+//    }
+//
+//    public static <T> BaseResponse<T> error(HttpStatus status, String msg) {
+//        return new BaseResponse<>(status, msg, null);
+//    }
+//}
 
